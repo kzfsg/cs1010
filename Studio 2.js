13173354538f0c2a9aa2c66a20e269cec833c830
Source Academy User <No public email provided>
2024-08-19 10:59:46 +0800

    first attempt studio 2

---
+++ Studio 2.js
@@ -0,0 +1,18 @@
+
+function biggie_size(x) {
+    return x + 4;
+}
+
+function unbiggie_size(x) {
+    return x - 4;
+}
+
+function is_biggie_size(x) {
+    return x > 4 ? true : false;
+}
+
+function combo
+
+biggie_size(1);
+unbiggie_size(5);
+is_biggie_size(5);
