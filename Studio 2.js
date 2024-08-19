0859f0364e3fb5aa4a6a71b14036963dd85b5687
Source Academy User <No public email provided>
2024-08-19 12:20:24 +0800

    Changes made from Source Academy

--- Studio 2.js
+++ Studio 2.js
@@ -1,18 +1,45 @@
-
+// q1
 function biggie_size(x) {
     return x + 4;
 }
 
+// q2
 function unbiggie_size(x) {
     return x - 4;
 }
 
+//q3
 function is_biggie_size(x) {
     return x > 4 ? true : false;
 }
 
-function combo
+//q4
+function combo_price(x) {
+    return (x > 4 ? (x-4) * 1.17 + 0.5 : x * 1.17);
+}
 
+//q5
+function empty_order() {
+    return 0;
+}
+
+//q6
+function add_to_order(x, y) {
+    return x * math_pow(10, (math_floor(math_log10(x)))) + y;
+}
+
+//q7
+function last_combo (x) {
+    return x % 10;
+}
+
+//q8
+function other_combos(x) {
+   return math_floor(x/10);  
+ }
+ 
 biggie_size(1);
 unbiggie_size(5);
 is_biggie_size(5);
+combo_price(8);
+add_to_order(12, 34);
