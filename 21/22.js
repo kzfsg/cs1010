61e21ade54187bf6c3240b474796add2e352f8ad
Source Academy User <No public email provided>
2024-09-28 13:17:55 +0800

    Changes made from Source Academy

---
+++ 21/22.js
@@ -0,0 +1,63 @@
+// Type your program in here!
+//1
+function repeat(v,k){
+    return k === 0
+    ? null
+    : pair(v, repeat(v, k-1));
+}
+
+//2
+function expand_list(L, k){
+    return accumulate((x,y) => append(repeat(x, k), y), null, L);
+}
+
+//3
+function expand_matrix(M, k){
+    return expand_list(
+        map(x => expand_list(x, k), M),
+        k);
+}
+
+//1. use expandlist to expand individual lists - use map to iterate through the lists?
+//2. repeat the list 3 times with repeat
+//
+
+function unique(xs) {
+if (is_null(xs) || is_null(tail(xs))) {
+    return xs;
+    } 
+    else {
+        return head(xs) !== head(tail(xs))
+        ? pair(head(xs), unique(tail(xs)))
+        : unique(tail(xs));
+    }
+}
+
+is_list(list(1,2));
+
+//11
+
+// function remove_elem(A, pos) {
+//     return x => x >= active_length(A)
+//                 ? null 
+//                 : x < pos
+//                 ? A(x)
+//                 : A(x+1);
+// }
+
+// function accumulate_tree(f1, f2, initial, tree) {
+// return is_null(tree)
+// ? initial
+// : f2( is_list(head(tree))
+// ? accumulate_tree(f1, f2, initial, head(tree))
+// : f1(head(tree)),
+// accumulate_tree(f1, f2, initial, tail(tree)));
+// }
+// const T = list(1,
+// list(2, 3, list(4, 5)),
+// 6,
+// list(list(7, 8), 9),
+// list(10)
+// );
+
+// accumulate_tree( x => x, (x, y) => x > y ? x : y, 7.5, T );
